956ceee2d2fd659fdf38e59a15a7f708c829b6ea
LivingLegacy95 <kgreene012@gmail.com>
2023-02-22 22:58:25 -0800

    DojoAssignment_ProfilePage

---
+++ script.js
@@ -0,0 +1,9 @@
+function hide(element) {
+    element.remove();
+}
+// if the clickable element is the same as the the element you wish to effect, then use"this"
+// if they're different then use the "id" of the element you wish to effect.
+
+function changeName(element){
+    element.innerText = "Tony The Tiger" 
+}
